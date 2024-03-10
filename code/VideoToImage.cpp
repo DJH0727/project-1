f233d37dc997372b47313e1be2c0213ecc19200e
DJH0727 <2724948893@qq.com>
2024-03-10 22:12:30 +0800

    ffmpeg

--- code/VideoToImage.cpp
+++ code/VideoToImage.cpp
@@ -1,10 +1,14 @@
 #include "VideoToImage.h"
+#include<iostream>
 int VideotoImage(const char* ffmPath, const char* imagePath,
 	const char* imageFormat, const char* videoPath)
 {
 	char systemCommand[256];
-	snprintf(systemCommand, 256, "\"%s\"ffmpeg.exe -i %s -f image2  %s\\%%04d.%s"
-	, ffmPath, videoPath, imagePath,imageFormat);
+	snprintf(systemCommand, 256, "%s\\ffmpeg.exe -i %s -f image2  %s\\%%04d.%s"
+		, ffmPath, videoPath, imagePath, imageFormat);
+	char CreatImagePath[20];
+	snprintf(CreatImagePath, 20, "md %s", imagePath);
+	system(CreatImagePath);
 	return system(systemCommand);
 }
 /*int main()
--- code/test.cpp
+++ code/test.cpp
@@ -1,7 +1,11 @@
+#define _CRT_SECURE_NO_WARNING
+#include<stdio.h>
 #include "ImageToVideo.h"
 #include "VideoToImage.h"
 int main()
 {
-	VideotoImage("", "test", "png", "out.mp4");
+	VideotoImage("bin", "image", "png", "test.mp4");
+	//ImagetoVideo("", 15, "image", "png", "out.mp4", 1000);
+	//VideotoImage("", "imageOut", "png", "out.mp4");
 	return 0;
 }
