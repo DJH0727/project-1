f9a08544f6d86b70b8424775d154a6a37a113077
Ly61 <14137175+liu-yi-3704@user.noreply.gitee.com>
2024-03-10 12:10:50 +0000

    删除文件 ImageToVideo.h

--- ImageToVideo.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <cstdlib>
-#include <cstring>
-#include <cstdio>
-int ImagetoVideo(const char* ffmPath, unsigned int videofps, const char* imagePath,
-	const char* imageFormat, const char* videoOutput, unsigned int bitRates);
