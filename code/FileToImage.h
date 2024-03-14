43d464893802f6a41303ccc81109edf002c3796d
Logic666666 <3582272218@qq.com>
2024-03-14 11:51:26 +0000

    更新FileToImage.h
    Signed-off-by: Logic666666 <3582272218@qq.com>

--- code/FileToImage.h
+++ code/FileToImage.h
@@ -1,20 +1,14 @@
-#ifndef QR_CODE_GENERATOR_H
-#define QR_CODE_GENERATOR_H
-
 #include <fstream>
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include <vector>
 
-#define QR_SIZE 128
-#define BLACK cv::Scalar(0, 0, 0)
-#define WHITE cv::Scalar(255, 255, 255)
-#define RECT_SIZE 24
+#define QR_SIZE 64  // 定义二维码的大小
+#define BLACK cv::Vec3b(0, 0, 0)  // 定义颜色黑色
+#define WHITE cv::Vec3b(255, 255, 255)  // 定义颜色白色
+#define RECT_SIZE 12  // 定义定位图案的大小
 
 int OpenFile(const std::string& filePath, std::vector<int>& data);
 void DrowRect(cv::Mat& image);
 void DrawQRPoint(const std::vector<int>& data, cv::Mat& image);
-cv::Mat BigMat(const cv::Mat& image);
+cv::Mat ScaleMat(const cv::Mat& image);
 void CreatQRcode(const std::string& filePath);
-
-#endif // QR_CODE_GENERATOR_H
